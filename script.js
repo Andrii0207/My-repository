e07a6827880fa7e2bcad8b815eb948b0d0d2b454
Andrii Popov <popov.andrey80@gmail.com>
2022-06-07 22:35:17 +0300

    save2

--- script.js
+++
@@ -1,8 +0,0 @@
-function splitMessage(message, delimeter) {
-  let words;
-  // Change code below this line
-  words = message.split(delimeter);
-  // Change code above this line
-  return words;
-}
-splitMessage('Mango hurries to the train', ' ');

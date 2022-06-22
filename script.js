46ddab5fe089414c658bbbd6bb94117d6d11b595
Andrii Popov <popov.andrey80@gmail.com>
2022-06-22 15:07:36 +0300

    Task 25 Module 4 (2 variance)

--- script.js
+++ script.js
@@ -1,16 +1,98 @@
-function getEvenNumbers(start, end) {
-  // Change code below this line
-  const evenNumberArray = [];
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37,
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34,
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24,
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21,
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27,
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38,
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39,
+  },
+];
 
-  for (let i = start; i <= end; i += 1) {
-    if (i % 2 === 0) {
-      evenNumberArray.push(i);
-    }
-  }
-  console.log(evenNumberArray);
-  return evenNumberArray;
+///////////////
 
-  // Change code above this line
-}
+// const getFriends = users => {
+//   return users.flatMap(user => user.friends);
+// };
+// console.log(getFriends(users));
 
-getEvenNumbers(3, 11);
+// const uniqueFriends = getFriends =>
+//   getFriends(users).filter((friend, index, array) => array.indexOf(friend) === index);
+// console.log(uniqueFriends(getFriends));
+
+//////////////////
+
+const getFriends = users.flatMap(user => user.friends);
+console.log(getFriends);
+const uniqueFriends = getFriends.filter((friend, index, array) => array.indexOf(friend) === index);
+console.log(uniqueFriends);
+
+///////////////////////
+
+// const getFriends = users =>
+//   users
+//     .flatMap(user => user.friends)
+//     .filter((friend, index, array) => array.indexOf(friend) === index);
+// console.log(getFriends(users));
